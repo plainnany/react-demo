f764b0fafdb1f413968f5c19c4099beb4efb2295
plainnany <plainnany@gmail.com>
2017-09-14 08:48:48 +0800

    仿照注册,完成登录功能

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
-        {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUp.bind(this)}/>}
+        {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUp.bind(this)}
+        onSignIn={this.onSignIn.bind(this)} />}
       </div>
     )
   }
@@ -86,7 +87,12 @@ class App extends Component {
     stateCopy.user = user
     this.setState(stateCopy)
   }
-  signOut(){
+  onSignIn(user){
+    let stateCopy = JSON.parse(JSON.stringify(this.state))
+    stateCopy.user = user
+    this.setState(stateCopy)
+  }
+  signOut(user){
     //signOut()
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = {}

f91ad43c952278c147ec33fb8a43dca187438fb2
plainnany <plainnany@gmail.com>
2017-09-16 22:51:36 +0800

    TodoInput组件修改为函数

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
 import './TodoInput.css'
 
-class TodoInput extends Component{
-    render() {
-        return <input type="text" value={this.props.content} 
-        className="TodoInput"
-        onChange={this.changeTitle.bind(this)}  
-        onKeyPress={this.submit.bind(this)} // 将submit中的this指的为TodoInput
-        />  
-        // onChange，onKeyPress是事件名
-    }
-    submit(e) {
-        if (e.key === 'Enter') {
-            if(e.target.value.trim() !== ''){
-                this.props.onSubmit(e)
-            }
-            
-        }
-    }
-
-    changeTitle(e) {
-        this.props.onChange(e)
+export default function(props){
+    return (
+        <input type="text" value={props.content} className="TodoInput"
+            onChange={changeTitle.bind(null,props)}  
+            onKeyPress={submit.bind(null,props)} 
+        />  // onChange，onKeyPress是事件名
+    )
+   
+}
+function submit(props,e) {
+    if (e.key === 'Enter') {
+        
+        props.onSubmit(e)
+        
+        
     }
 }
 
-export default TodoInput
+function changeTitle(props,e) {
+    props.onChange(e)
+}
